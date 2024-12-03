1f2821c660007aa4b1fcf34fc99b09642f56bb82
ymyeemon <23008873@myrp.edu.sg>
2024-12-03 15:00:51 +0800

    new commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,4 +5,18 @@ import React from "react";
  * A single movie item in the movie list, with a checkbox to mark it
  * watched, a text span for the title, and a delete button.
  */
-export default function Movie() {}
+export default function Movie({ movie, onToggleWatched, onDeleteMovie }) {
+    return (
+        <li>
+  
+        <input type="checkbox"
+        value={movie.watched}
+        onChange={() => onToggleWatched(movie.id)}/>
+  
+        <span style={movie.watched ? {textDecoration: "line-through"} : {}}>{movie.title}</span>
+  
+        <button onClick={() => onDeleteMovie(movie.id)}>Delete</button>
+  
+      </li>
+    )
+}
